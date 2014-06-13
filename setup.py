36df33157a496503a2ada074df3b77703091f81d
Corey Richardson <corey@octayn.net>
2014-06-12 17:24:35 -0700

    windows-only: use pbs instead of sh
    Closes #24

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python
 
+import platform
 from setuptools import setup
 
+sh = 'sh'
+
+if platform.system() == 'Windows':
+    sh = 'pbs'
+
 setup(name='cargo-lite',
-      version='1.1.4',
+      version='1.1.5',
       description='Rust package manager',
       author='Corey Richardson',
       author_email='corey@octayn.net',
       url='http://github.com/cmr/cargo-lite',
       scripts=['cargo-lite'],
-      install_requires=['docopt ', 'toml', 'sh', 'pyblake2', 'colorama']
+      install_requires=['docopt ', 'toml', sh, 'pyblake2', 'colorama']
       )
